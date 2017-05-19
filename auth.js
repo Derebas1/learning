88f1c6bbf0dc8356c3cda59bed47203373a032ea
Derebas1 <deryugin-dmitry2014@yandex.ru>
2017-05-19 15:52:52 +0300

    Changed auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate (login, password) {
-	if (login === 'login' && password === 'password')
+	if (login === 'login'){
 		return 'You were lagged in';
 } else {
-	return 'Login is incorrect';
+	return 'Login is correct';
 }
